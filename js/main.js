c659aef59ddf761011583ec94bc7a47c9b561991
NeuralNat <ifrance@yandex.ru>
2023-11-28 15:11:16 +0300

    JS closeHints() by click at document

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const infoBtns = document.querySelectorAll('.info-dot');
+const infoHints = document.querySelectorAll('.info-hint');
 
+//Клик по кнопкам с подсказками
 for (let btn of infoBtns) {
     btn.addEventListener('click', showHint);
 }
@@ -9,4 +11,13 @@ function showHint() {
     console.log(this);
     console.log(this.parentNode);
     this.parentNode.querySelector('.info-hint').classList.toggle('none');
+}
+
+// Закрываем подсказку при клика по документу 
+document.addEventListener('click', closeHints);
+
+function closeHints () {
+for (let hint of infoHints) {
+    hint.classList.add('none');
+}
 }
