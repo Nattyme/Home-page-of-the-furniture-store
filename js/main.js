0ee92ccfd3b59f4bc521c20be0a6b5dac668dba8
NeuralNat <ifrance@yandex.ru>
2023-11-30 14:49:55 +0300

    JS .tabs-controls click listening

--- js/main.js
+++ js/main.js
@@ -48,3 +48,18 @@ const swiper = new Swiper('.swiper', {
   //   },
   // },
 });
+
+// Tabs
+const tabsBtns = document.querySelectorAll('[data-tab]');
+for( let btn of tabsBtns) {
+  btn.addEventListener('click', function() {
+
+    // Убираем активные классы у всех кнопок
+    for( let btn of tabsBtns) {
+      btn.classList.remove('tab-controls__btn--active');
+    }
+    // Добавляем активный класс к текущей кнопке
+  this.classList.add('tab-controls__btn--active');
+  });
+}
+
